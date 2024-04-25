d37dbcbf1f828be33f86836acebfbadb9b3d2d48
Nabin <102199897+nabpar@users.noreply.github.com>
2024-04-25 15:32:56 +0545

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,6 @@ import { BACKEND_DOMAIN } from "./constants";
 const BASE_URL=BACKEND_DOMAIN
 
 export default axios.create({
-  // baseURL: "https://napa.pythonanywhere.com/",
+   baseURL: "https://napa.pythonanywhere.com/",
   baseURL: BASE_URL
 });
